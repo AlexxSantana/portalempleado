ee0ed0b3eab133bf9cabab27ad0123c2da9b56d9
AlexxSantana <71818252+AlexxSantana@users.noreply.github.com>
2021-02-23 21:04:10 +0100

    Add files via upload

---
+++ models/altaEmpleado_model.php
@@ -0,0 +1,19 @@
+<?php
+//La lógica vendrá aquí, funciones que se comunican con la bbdd
+# Función 'listaProductos'. 
+# Parámetros: 
+# 	
+# Funcionalidad: Se prepara una consulta sql del productName y que la quantityInStock sea superior a 0
+# 
+# Return: lista de los productos
+#
+# Alex Santana
+function listaDepa(){
+	global $conexion;
+	$sql="SELECT dept_no, dept_name FROM departments";
+	$stmt=$conexion->prepare($sql);
+	$stmt->execute();
+	$listadepa=$stmt->fetchAll(PDO::FETCH_ASSOC);
+	return $listadepa;
+}
+?>
