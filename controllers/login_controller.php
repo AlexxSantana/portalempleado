ae16ff5f05fca53b869806030fd7eeb4e196b0b5
AlexxSantana <71818252+AlexxSantana@users.noreply.github.com>
2021-02-23 21:00:26 +0100

    Update login_controller.php

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -2,6 +2,8 @@
 <style>
 .error{
 	color:red;
+	position:absolute;
+	top:29%;
 }
 </style>
 <?php
@@ -10,7 +12,7 @@ require_once("models/login_model.php");
 	
 //Se incia la sesion
 session_start();
-//Si no existe 
+
 
 if(isset($_SESSION["usuario"])){
 		session_unset();
@@ -20,23 +22,25 @@ if(isset($_SESSION["usuario"])){
 if (isset($_POST) && !empty($_POST)) {
 	$user=$_POST["usuario"];
 	$contra=$_POST["contra"];
-
+	
 	$consulta=validarDatos($user, $contra);
 	
 	$empleadoPerteneceRH=empleadoRH($user);
-	if($empleadoPerteneceRH!=null){
-		//Si existe el usuario en la bbdd se crea la sesion usuario con el emp_no (id) y redirigirá al menu de empleado de HUman Recur
+
+		//Si existe el usuario en la bbdd se crea la sesion usuario con el emp_no (id) y redirigirá al menu de empleado de HUman Recur o al menú de empleado normal
 		if($consulta!=null){
-			$_SESSION["usuario"]=$consulta["emp_no"]; 
-			header("location: views/menuEHR_view.php"); 
+			$_SESSION["usuario"]=$consulta["emp_no"];
+			if($empleadoPerteneceRH!=null){
+				header("location: views/menuEHR_view.php"); 
+			}else{//Cualquier empleado que no pertenezca a RH Redirigirá al siguiente menú
+				header("location: views/menuEN_view.php"); 
+			}
 		} else{
-			
 			echo "<p class='error'>Datos Incorrectos</p>";
 		}
-	} else{
-		echo "<p>El empleado no pertenece al dp RH</p>";
-	}
 }
+
+
 
 //Llamada a la vista, intermediario entre vista y modelo
 require_once("views/login_view.php");
