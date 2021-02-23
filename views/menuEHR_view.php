442a29a436d6b3b263f850e10d7ff9f0d597baef
AlexxSantana <71818252+AlexxSantana@users.noreply.github.com>
2021-02-23 21:02:09 +0100

    Update menuEHR_view.php

--- views/menuEHR_view.php
+++ views/menuEHR_view.php
@@ -4,21 +4,23 @@ session_start();
 //Si empleado no existe, es decir no esta logeado, volverá al index.php para logearse y no se podrá ver la info de esta pag hasta que se logee correctamente
 if(!isset($_SESSION["usuario"])){
 		header("location: ../index.php");
+}else{
+		$idUser=$_SESSION["usuario"];
+		$nombreU=obtenerNombre($idUser);
 	}
 	
 ?>
-
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Portal del Empleado</title>
 	<meta charset="utf-8" />
-	<meta name="author" value="Lucas Fadavi" />
+	<meta name="author" value="Alex Santana" />
 </head>
 <body>
-	<h1>Bienvenido Empleado - Perteneces a Recursos Humanos<!--<?php echo htmlspecialchars($_COOKIE["user"]) ?>--></h1>
-	<li><a href="">Alta Empleado</a></li>
+	<h1>Bienvenido <?php echo $nombreU["first_name"]." ".$nombreU["last_name"];?> - DPTO Recursos Humanos</h1>
+	<li><a href="altaEmpleado_view.php">Alta Empleado</a></li>
 	<li><a href="">Alta Masiva de Empleados</a></li>
 	<li><a href="">Modificar salario</a></li>
 	<li><a href="">Vida Laboral</a></li>
@@ -26,6 +28,25 @@ if(!isset($_SESSION["usuario"])){
 	<li><a href="">Cambio Departamento</a></li>
 	<li><a href="">Nuevo Jefe Departamento</a></li>
 	<li><a href="">Baja Empleado</a></li>
+	<li><a href="../index.php">Cerrar Sesión</a></li>
 </body>
 </html>
+<?php
+# Función 'listaProductos'. 
+# Parámetros: 
+# 	
+# Funcionalidad: Se prepara una consulta sql del productName y que la quantityInStock sea superior a 0
+# 
+# Return: lista de los productos
+#
+# Alex Santana
+function obtenerNombre($usuario){
+	include_once("../db/db.php");
+	$sql="SELECT first_name, last_name FROM employees WHERE emp_no='$usuario'";
+	$stmt=$conexion->prepare($sql);
+	$stmt->execute();
+	$nombre=$stmt->fetch(PDO::FETCH_ASSOC);
+	return $nombre;
+}
+?>
 
