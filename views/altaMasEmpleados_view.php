8e0bea8976d5062076d6e30c71cd0a0b074f8eaa
AlexxSantana <71818252+AlexxSantana@users.noreply.github.com>
2021-02-24 22:26:42 +0100

    Add files via upload

--- views/altaMasEmpleados_view.php
+++ views/altaMasEmpleados_view.php
@@ -2,16 +2,6 @@
 require_once("../db/db.php");
 include_once("../controllers/altaMasEmpleados_controller.php");
 
-
-//Si empleado no existe, es decir no esta logeado, volverá al index.php para logearse y no se podrá ver la info de esta pag hasta que se logee correctamente
-/*if(!isset($_SESSION["usuario"])){
-		header("location: ../index.php");
-		session_destroy();
-}else{
-		$idUser=$_SESSION["usuario"];
-		$nombreU=obtenerNombre($idUser);
-	}*/
-
 ?>
 
 <!DOCTYPE html>
@@ -29,42 +19,65 @@ div#uno{
 }
 input[type="text"]{
     position: absolute;
-    left: 105%;
+    left: 12%;
 }
 
 input[type="date"]{
     position: absolute;
-    left: 105%;
+    left: 12%;
 }
 
 input[type="number"]{
     position: absolute;
-    left: 105%;
+    left: 12%;
 }
 
 select{
 	position: absolute;
-    left: 105%;
+    left: 12%;
 }
 
 .user{
 	color:brown;
 	font-size:18pt;
 }
+
+table {border-collapse: collapse;
+	margin:0 auto;
+		}
+	th, td {border: 1px solid #dddddd;
+			width:auto;
+			text-align:center;}
+.tit{
+	position:relative;
+	left:45%;
+	
+}
+
+.mover{
+	position:relative;
+	left:45%;
+}
+
+.error{
+	color:red;
+	position:absolute;
+	top:64%;
+}
 </style>
 <body>
 	<h3>Usuario <strong class="user"><?php echo $nombreU["first_name"]." ".$nombreU["last_name"];?></strong></h3>
     <h1> Alta de Empleados</h1>
-	<div id="uno">
+	<!--<div id="uno">-->
     <form name='datos' action='<?php echo htmlentities($_SERVER["PHP_SELF"]);?>' method='POST'>
         <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" required /><br><br>
+        <input type="text" name="nombre"  /><br><br>
 
         <label for="apellido">Apellido</label>
         <input type="text" name="apellido" /><br><br>
 		
 		 <label for="nacido">Fecha Nacimiento</label>
-        <input type="date" name="nacido" required /><br><br>
+        <input type="date" name="nacido"  /><br><br>
 		
 		 <label for="genero">Género</label>
         <select name="genero">
@@ -103,8 +116,22 @@ select{
 
         <input type="submit" name="agregar" value="Agregar empleado"/>
 		<a href="menuEHR_view.php" ><input type="button" value="Volver al menú" class="volver"></a>
+		<input type="submit" name="ver" value="Ver empleados"/>
     </form>
-	</div>
+	<?php
+		if(isset($_POST["ver"])){
+			if(!empty($_SESSION["bandejaempleados"])){
+				tablaEmpleados($listaempleados);
+				echo "<form action='' method='post'>";
+					echo '<input class="mover" type="submit" name="vaciar" value="Eliminar registros">';
+				echo "</form>";
+			} else{
+				echo "<p class='error'>No se ha agregado a ningún empleado a la lista<p>";
+			}
+		}
+	?>
+	<!--</div>-->
+	
 </body>
 </html>
 
