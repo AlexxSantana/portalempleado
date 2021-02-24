6b4df80d4e36d233e865832341e0470563407d06
AlexxSantana <71818252+AlexxSantana@users.noreply.github.com>
2021-02-24 19:57:24 +0100

    Add files via upload

---
+++ controllers/altaMasEmpleados_controller.php
@@ -0,0 +1,52 @@
+<meta charset="utf-8" />
+<style>
+.error{
+	color:red;
+	position:absolute;
+	top:39%;
+}
+
+.exito{
+	color:blue;
+	position:absolute;
+	top:65%;
+}
+
+</style>
+<?php
+//Llamada al modelo, intermediario entre vista y modelo
+require_once("../models/altaMasEmpleados_model.php");
+	
+//Se incia la sesion
+session_start();
+
+/*if(isset($_SESSION["usuario"])){
+		session_unset();
+		session_destroy();
+}*/
+	
+	
+if(!empty($_SESSION["usuario"])){
+	
+	$listadpt=listaDepa();
+	$listacargo=listaCargo();
+}
+//si se hace click en el botón de dar Alta se insertarán los datos
+if (isset($_POST["alta"])){
+	//Se obtiene los valores del formulario (altaEmpleado_view.php)
+	/*$nacido=$_POST["nacido"];
+	$nombre=$_POST["nombre"];
+	$apellido=$_POST["apellido"];
+	$genero=$_POST["genero"];
+	$contrato=$_POST["contrato"];
+	$depa=$_POST["departamento"];
+	$salario=$_POST["salario"];
+	$cargo=$_POST["cargo"];
+	//Insertar datos en la tabla employees
+	altaEmpleado($nacido, $nombre, $apellido, $genero, $contrato, $depa, $salario, $cargo);*/
+	//echo "<p class='exito'>Se ha dado de alta un/a nuevo empleado/a</p>";
+}
+
+//Llamada a la vista, intermediario entre vista y modelo
+require_once("../views/altaMasEmpleados_view.php");
+?>
