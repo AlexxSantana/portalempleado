b297aa3b13568017e80335de6c9c228389c0e00d
AlexxSantana <71818252+AlexxSantana@users.noreply.github.com>
2021-02-24 20:42:27 +0100

    Add files via upload

--- controllers/altaMasEmpleados_controller.php
+++ controllers/altaMasEmpleados_controller.php
@@ -24,17 +24,33 @@ session_start();
 		session_unset();
 		session_destroy();
 }*/
-	
-	
+
+//Si empleado no existe, es decir no esta logeado, volverá al index.php para logearse y no se podrá ver la info de esta pag hasta que se logee correctamente
+if(!isset($_SESSION["usuario"])){
+		header("location: ../index.php");
+		//session_destroy();
+}else{
+		$idUser=$_SESSION["usuario"];
+		$nombreU=obtenerNombre($idUser);
+	}
+
+//	
 if(!empty($_SESSION["usuario"])){
 	
 	$listadpt=listaDepa();
 	$listacargo=listaCargo();
 }
+
+
+//
+
+
+
+
 //si se hace click en el botón de dar Alta se insertarán los datos
-if (isset($_POST["alta"])){
+if (isset($_POST["agregar"])){
 	//Se obtiene los valores del formulario (altaEmpleado_view.php)
-	/*$nacido=$_POST["nacido"];
+	$nacido=$_POST["nacido"];
 	$nombre=$_POST["nombre"];
 	$apellido=$_POST["apellido"];
 	$genero=$_POST["genero"];
@@ -43,8 +59,14 @@ if (isset($_POST["alta"])){
 	$salario=$_POST["salario"];
 	$cargo=$_POST["cargo"];
 	//Insertar datos en la tabla employees
-	altaEmpleado($nacido, $nombre, $apellido, $genero, $contrato, $depa, $salario, $cargo);*/
+	/*altaEmpleado($nacido, $nombre, $apellido, $genero, $contrato, $depa, $salario, $cargo);*/
 	//echo "<p class='exito'>Se ha dado de alta un/a nuevo empleado/a</p>";
+	if(!isset($_SESSION["bandejaempleados"])){
+		$_SESSION["bandejaempleados"]=array(array($nacido, $nombre, $apellido, $genero, $contrato, $depa, $salario, $cargo));
+	}else{
+		array_push($_SESSION["bandejaempleados"],array($nacido, $nombre, $apellido, $genero, $contrato, $depa, $salario, $cargo));
+	}
+	var_dump($_SESSION["bandejaempleados"]);
 }
 
 //Llamada a la vista, intermediario entre vista y modelo
